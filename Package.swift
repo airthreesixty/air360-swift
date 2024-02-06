d79b34195b11377ff8ca7bca95622922b2bde43f
Hung Quach <hungquach12@outlook.com>
2024-02-06 16:54:08 +0700

    0.4.8

--- Package.swift
+++ Package.swift
@@ -18,5 +18,10 @@ let package = Package(
       url: "https://cdn.air360tracker.net/ios/air360-sdk-0.4.7.zip",
       checksum: "e2740e8ae7b0f67576ddf1667197d5b987302b8cbe85ec95fa145b115ea7c97e"
     )
+    .binaryTarget(
+      name: "Air360",
+      url: "https://cdn.air360tracker.net/ios/air360-sdk-0.4.8.zip",
+      checksum: "7354385185dadbfee36ff3b8274c405e6370cfb97229163874dedc99070bbe03"
+    )
   ]
 )
